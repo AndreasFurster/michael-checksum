876c0519a76e8ce125d425843b475365c41f0156
Andreas Furster <andreas.furster@hotmail.com>
2018-09-06 20:24:21 +0200

    Hello world is really hard... Still not working

---
+++ src/Dashboard.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Dashboard extends JFrame {
+    private JTextField hellowWorldField;
+    private JPanel panel1;
+
+    Dashboard(){
+        this.hellowWorldField.setText("Hello world");
+    }
+}
