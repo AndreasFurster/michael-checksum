7c65e85004b3f367f3bc594130cd125aca3caacc
DESKTOP-Q3BTUMG\royst <Roy@delcom.nl>
2018-09-20 19:25:16 +0200

    ADD: FileWatcher on the downloads map: userhome + downloads

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
-
 public class Main {
 
     public static void main(String[] args) {
         // Dashboard d = new Dashboard();
         // d.setVisible(true);
-        
+        FileWatcher fileWatcher = new FileWatcher();
+        fileWatcher.StartWatching();
     }
 }
