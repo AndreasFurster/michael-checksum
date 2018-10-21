57dc9d7304f6d7bb32038a75d7200dff8b993c49
Andreas Furster <andreas.furster@hotmail.com>
2018-10-21 20:33:18 +0200

    Adding info for file watching

--- src/com/michaelchecksum/business/FileValidationManager.java
+++ src/com/michaelchecksum/business/FileValidationManager.java
@@ -1,4 +1,10 @@
 package com.michaelchecksum.business;
 
 public class FileValidationManager implements FileEventListener {
+
+
+    @Override
+    public void NewFileFound() {
+        // TODO: Handle new file result (open popup)
+    }
 }
--- src/com/michaelchecksum/business/FileWatchingManager.java
+++ src/com/michaelchecksum/business/FileWatchingManager.java
@@ -2,13 +2,15 @@ package com.michaelchecksum.business;
 
 public class FileWatchingManager {
 
-    public void attatchResponer(FileEventListener fileValidationManager) {
+    public void attatchResponer(FileEventListener responder) {
+        // TODO: Add responder to responder list
     }
 
     public void startWatchers() {
+        // TODO: Call all responders in responder list
     }
 }
 
 interface FileEventListener {
-
+    void NewFileFound();
 }
