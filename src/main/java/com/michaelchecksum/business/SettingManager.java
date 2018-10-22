358c39fbcb347e8dac3763c85cb6969e2c8db240
Roy Stijsiger <Roy@delcom.nl>
2018-10-22 21:19:12 +0200

    Add: GetFolders and GetUsername from settings.json

--- src/main/java/com/michaelchecksum/business/SettingManager.java
+++ src/main/java/com/michaelchecksum/business/SettingManager.java
@@ -2,36 +2,53 @@ package com.michaelchecksum.business;
 
 import com.michaelchecksum.data.SettingStorage;
 import com.michaelchecksum.domain.viewmodels.SettingsViewModel;
-import com.michaelchecksum.presentation.Settings;
+import com.michaelchecksum.presentation.SettingsUi;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class SettingManager {
 
     private ArrayList<Path> settings = new ArrayList<>();
-    private SettingStorage  settingStorage = new SettingStorage();
-    private Settings gui;
+    private SettingStorage  settingStorage = new SettingStorage(Paths.get(System.getProperty("user.dir") + "\\settings.json"));
+    private SettingsUi gui;
     private SettingsViewModel viewModel;
 
     public SettingManager() {
-        this.gui = new Settings();
+        this.gui = new SettingsUi();
         this.viewModel = new SettingsViewModel();
 
         this.getSettings();
     }
 
+    public void addPathToSettings(String path){
+        System.out.println(path);
+    }
 
     public void openSettings() {
         this.viewModel.setPaths(this.getSettings());
+        this.viewModel.setOnSettingsAddEventHandler(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t ) {
+                // TODO: HANDLE IT PROPERLY;
+                Button btn = (Button) t.getSource(); 
+                String path = btn.getId();
+                addPathToSettings(path);
+            }
+        });
         this.gui.initializeComponent(this.viewModel);
         this.gui.show();
     }
 
-    public ArrayList<Path> getSettings() {
+
+    public ArrayList<String> getSettings() {
         try {
-            ArrayList<Path> listOfPaths = settingStorage.getFilewatchPaths();
+            ArrayList<String> listOfPaths = settingStorage.getFilewatchPaths();
             return listOfPaths;
         }
         catch(IOException exception){
--- src/main/java/com/michaelchecksum/data/SettingStorage.java
+++ src/main/java/com/michaelchecksum/data/SettingStorage.java
@@ -1,35 +1,64 @@
 package com.michaelchecksum.data;
 
 import com.google.gson.Gson;
+import com.michaelchecksum.domain.Settings;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class SettingStorage {
     private Path location;
+    private File file;
     private Gson gson = new Gson();
     private ArrayList<Path> paths = new ArrayList<>();
 
+    public SettingStorage(Path location) {
+        this.location = location;
+        this.file = new File(location.toString());
+        try {
+            if(!this.file.exists()) {
+                this.file.createNewFile();
+                List<String> lines = new ArrayList<>();
+                lines.add("{\"folders\":[], \"username\" : \"\"}");
+                Files.write(this.location, lines);
+            }
+        }
+        catch(IOException ex){
+            // TODO: handle the exception
+        }
+
+    }
+
     public boolean InsertFilewatchPath(Path path){
-        //get paths first
+        // TODO: INSERTFILEWATCHPATH
 
         return true;
     }
 
     public boolean DeleteFilewatchPath(Path path) {
-        //delete
+        // TODO: DELETEFILEWATCHPATH
         return true;
     }
 
-    public ArrayList<Path> getFilewatchPaths() throws IOException{
+    public String getUsername() throws IOException{
+        String content = new String(Files.readAllBytes(Paths.get("settings.json")));
 
-        //String content = new String(Files.readAllBytes(Paths.get("settings.json")));
+        Settings settings = gson.fromJson(content, Settings.class);
 
-        ArrayList<Path> paths = new ArrayList<>();
-        paths.add(Paths.get(System.getProperty("user.home") + "\\Downloads\\"));
-        return paths;
+        return settings.username;
+    }
+
+    public ArrayList<String> getFilewatchPaths() throws IOException{
+
+        String content = new String(Files.readAllBytes(Paths.get("settings.json")));
+
+        Settings settings = gson.fromJson(content, Settings.class);
+
+        return settings.folders;
     }
 }
