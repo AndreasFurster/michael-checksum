7ec1aba3c1ef4a9f8f4723c0ed04660577d0770c
Andreas Furster <andreas.furster@hotmail.com>
2018-10-22 17:46:42 +0200

    Make it buildable...

--- src/main/java/com/michaelchecksum/data/Database.java
+++ src/main/java/com/michaelchecksum/data/Database.java
@@ -5,13 +5,13 @@ public class Database {
     private Connection connection;
 
     public Database(){
-        try {
-            this.connection = DriverManager.getConnection("jdbc:mariadb://185.182.56.176:3306/andredg46_proftaak21", "andredg46_proftaak21", "NgOuhEjW");
-
-        } catch(SQLException  ex) {
-            ex.printStackTrace();
-            throw new RuntimeException(ex.getMessage());
-        }
+//        try {
+//            this.connection = DriverManager.getConnection("jdbc:mariadb://185.182.56.176:3306/andredg46_proftaak21", "andredg46_proftaak21", "NgOuhEjW");
+//
+//        } catch(SQLException  ex) {
+//            ex.printStackTrace();
+//            throw new RuntimeException(ex.getMessage());
+//        }
     }
 
     public Connection getConnection(){
