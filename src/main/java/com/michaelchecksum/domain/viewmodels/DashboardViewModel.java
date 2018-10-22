f031b1434dd072863be529a78728735b7e203418
Roy Stijsiger <Roy@delcom.nl>
2018-10-22 19:14:59 +0200

    add: settings butotn and add functionality

--- src/main/java/com/michaelchecksum/domain/viewmodels/DashboardViewModel.java
+++ src/main/java/com/michaelchecksum/domain/viewmodels/DashboardViewModel.java
@@ -2,6 +2,8 @@ package com.michaelchecksum.domain.viewmodels;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 // TODO: success, failed and total counter values can be calculated
 // TODO: success, failed and total don't have to be bidirectional
@@ -9,6 +11,8 @@ public class DashboardViewModel {
     private DoubleProperty successAmount = new SimpleDoubleProperty();
     private DoubleProperty failedAmount = new SimpleDoubleProperty();
     private DoubleProperty totalAmount = new SimpleDoubleProperty();
+
+    private EventHandler<MouseEvent> onSettingsClick;
 
     public DoubleProperty failedAmountProperty() {
         return failedAmount;
@@ -34,5 +38,12 @@ public class DashboardViewModel {
         return successAmount;
     }
 
+    public void setOnSettingsClickEventHandler(EventHandler<MouseEvent> eventHandler) {
+        this.onSettingsClick = eventHandler;
+    }
+
+    public EventHandler<MouseEvent> onSettingsClick() {
+        return this.onSettingsClick;
+    }
 
 }
